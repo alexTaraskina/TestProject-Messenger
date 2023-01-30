ec663b8fc56507cd72289f56e1743574d5223f3d
alexTaraskina <serzhanina_aleks@mail.ru>
2023-01-30 21:18:28 +0300

    profile link and chats link

--- src/partials/modules/chats/chats.ts
+++ src/partials/modules/chats/chats.ts
@@ -1,12 +1,38 @@
-import { Block } from '../../../core';
+import { Block, CoreRouter, Store } from '../../../core';
 import template from 'bundle-text:./template.hbs';
+import { withRouter, withStore } from 'utils';
+import { logout } from 'services/auth';
 
 import './chats.css';
 
-export default class Chats extends Block {
+interface ChatsProps {
+    router: CoreRouter,
+    store: Store<AppState>,
+    onProfileLinkClick?: (e: Event) => void,
+    events: {
+        submit: (event: MouseEvent) => void
+    }
+}
+
+class Chats extends Block<ChatsProps> {
     static componentName: string = 'Chats';
+
+    constructor(props: ChatsProps) {
+        super(props);
+
+        this.setProps({
+            onProfileLinkClick: (e: Event) => this.onProfileLinkClick(e),
+        });
+    }
+
+    onProfileLinkClick(e: Event) {
+        e.preventDefault();
+        this.props.router.go("/profile");
+    }
     
     render() {
         return template;
     }
 }
+
+export default withStore(withRouter(Chats));
--- src/partials/modules/profile-card/profile-card.ts
+++ src/partials/modules/profile-card/profile-card.ts
@@ -1,12 +1,39 @@
-import { Block } from '../../../core';
+import { Block, Store, CoreRouter } from '../../../core';
 import template from 'bundle-text:./template.hbs';
+import { withRouter, withStore } from 'utils';
+import { logout } from 'services/auth';
 
 import './profile-card.css';
 
-export default class ProfileCard extends Block {
+interface ProfileCardProps {
+    store: Store<AppState>,
+    router: CoreRouter,
+    onLogoutClick?: (e: Event) => void,
+    events: {
+        submit: (event: MouseEvent) => void
+    }
+}
+
+class ProfileCard extends Block<ProfileCardProps> {
     static componentName: string = 'ProfileCard';
+
+    constructor(props: ProfileCardProps) {
+        super(props);
+
+        this.setProps({
+            onLogoutClick: (e: Event) => this.onLogoutClick(e),
+        });
+    }
+
+    onLogoutClick(e: Event) {
+        e.preventDefault();
+        this.props.store.dispatch(logout);
+        this.props.router.go('/login');
+    }
     
     render() {
         return template;
     }
 }
+
+export default withRouter(withStore(ProfileCard));
