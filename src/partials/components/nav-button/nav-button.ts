d5256c2be3563a54e8a13b08c77beb80694f054f
alexTaraskina <serzhanina_aleks@mail.ru>
2023-01-31 19:06:10 +0300

    nav button logic

--- src/partials/components/nav-button/nav-button.ts
+++ src/partials/components/nav-button/nav-button.ts
@@ -4,13 +4,21 @@ import template from 'bundle-text:./template.hbs';
 import './nav-button.css';
 
 interface NavButtonProps {
+    events: {
+        click: () => void
+    }
 }
 
 export default class NavButton extends Block<NavButtonProps> {
     static componentName: string = 'NavButton';
 
     constructor(props: NavButtonProps) {
-        super(props);
+        super({
+            ...props,
+            events: {
+                click: () => window.history.back()
+            }
+        });
     }
 
     render() {
