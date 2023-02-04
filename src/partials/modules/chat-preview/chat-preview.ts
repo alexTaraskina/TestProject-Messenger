fbc6118a67c31decf3310bd14ee8e42a1fa89c87
alexTaraskina <serzhanina_aleks@mail.ru>
2023-02-04 17:18:46 +0300

    go to chat page

--- src/partials/modules/chat-preview/chat-preview.ts
+++ src/partials/modules/chat-preview/chat-preview.ts
@@ -1,12 +1,35 @@
-import { Block } from '../../../core';
+import { Block, CoreRouter } from 'core';
 import template from 'bundle-text:./template.hbs';
 
 import './chat-preview.css';
+import { withRouter } from 'utils';
 
-export default class ChatPreview extends Block {
+interface ChatPreviewProps {
+    id: number,
+    title: string,
+    router: CoreRouter,
+    onChatTitleClick?: (e: Event) => void,
+}
+
+class ChatPreview extends Block<ChatPreviewProps> {
     static componentName: string = 'ChatPreview';
+
+    constructor(props: ChatPreviewProps) {
+        super(props);
+
+        this.setProps({
+            onChatTitleClick: (e: Event) => this.onChatTitleClick(e),
+        });
+    }
+
+    onChatTitleClick(e: Event) {
+        e.preventDefault();
+        this.props.router.go(`/messenger/${this.id}`);
+    }
 
     render() {
         return template;
     }
 }
+
+export default withRouter(ChatPreview);
