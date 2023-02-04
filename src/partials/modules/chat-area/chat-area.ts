da8c47765583c911ad2b698a55dbc8d7b1990444
alexTaraskina <serzhanina_aleks@mail.ru>
2023-02-04 18:08:42 +0300

    show options popup

--- src/partials/modules/chat-area/chat-area.ts
+++ src/partials/modules/chat-area/chat-area.ts
@@ -3,8 +3,39 @@ import template from 'bundle-text:./template.hbs';
 
 import './chat-area.css';
 
-export default class ChatArea extends Block {
+interface ChatAreaProps {
+    onChoseOptionClick?: (e: Event) => void,
+}
+
+export default class ChatArea extends Block<ChatAreaProps> {
     static componentName: string = 'ChatArea';
+
+    constructor(props: ChatPreviewProps) {
+        super(props);
+
+        this.setProps({
+            onChoseOptionClick: (e: Event) => this.onChoseOptionClick(e),
+        });
+
+        let actionButton = this.element?.querySelector('#actionsButton');
+        if (actionButton) {
+            actionButton.addEventListener('click', this.onChoseOptionClick);
+        }
+
+        let attachFileButton = this.element?.querySelector('#attachFileButton');
+        if (attachFileButton) {
+            attachFileButton.addEventListener('click', this.onChoseOptionClick);
+        }
+    }
+
+    onChoseOptionClick(e: Event) {
+        e.preventDefault();
+        let el = e.target as HTMLElement;
+        if (el && el.parentNode) {
+            let options = el.parentNode.querySelector('.jsOptions');
+            options?.classList.toggle('active');
+        }
+    }
 
     render() {
         return template;
