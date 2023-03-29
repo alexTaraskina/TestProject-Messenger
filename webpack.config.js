47f97908cc8623f8a3fd4c6f5ed13d1daaa75d7a
alexTaraskina <serzhanina_aleks@mail.ru>
2023-03-29 23:18:21 +0300

    postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
                 use: [
                     'style-loader',
                     'css-loader',
+                    'postcss-loader',
                 ],
             },
             {
